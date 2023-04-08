e89b5e9c8add779e28df4a4342ce13e53566f317
JJ <jenniferj1796@gmail.com>
2023-04-08 19:32:40 -0400

    added JS code and CSS

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,130 @@
-var beginBtn = document.getElementById("start");
-var timer = document.getElementById("countdown");
-var rightAnswer = document.getElementById("optionb")
-var tiktoc =90;
-var theQuestions= []
-   
-beginBtn.addEventListener("click", startCount);
+var startbutton = document.querySelector(".start");
+var guidelines = document.querySelector(".intro");
+var nextbutton = document.querySelector(".next");
+let timeremaining = 90;
+var countDown = document.querySelector(".countdown");
+var timeout = document.querySelector(".quizsection");
+var scoresheet = document.querySelector(".submitscores");
+var scorehistory = document.querySelector (".leaderboard");
+var attempt = 0
 
-function startCount() {
-console.log("This works");
+var theQuestions = [
+    {question: "What symbol is an array inside of?",
+            a:"/ /",
+            b:"[ ]",
+            c:"{ }",
+            d:";",
+            correct:"b",
+        },
+    
+    {question: "What is the proper way to redeclare this variable? var studentCount = 3",
+            a:"var student = 4",
+            b:"studentCount = 4",
+            c:"countStudent = 4",
+            d:"count+1",
+            correct:"b",
+        },
+    
+    {question: "Which of the following is a boolean", 
+            a:"9",
+            b:"True",
+            c:"String",
+            d:"Variable",
+            correct:"b",
+        },
+    
+    {question: "How do you prevent default?", 
+            a:"stop",
+            b:"event.preventDefault();",
+            c:"return;",
+            d:"event",
+            correct:"b",
+          },
 
-    var timeInterval = setInterval(function () {
-     tiktoc--;
-     timer.textContent=" Time: "+tiktoc+" seconds";
-  
-     if(tiktoc===0) {
-      clearInterval(timeInterval);
-     }
-    }, 1000);
-  }
-  
+]
+
+var a_text = document.getElementById("a_text");
+var b_text = document.getElementById("b_text");
+var c_text = document.getElementById("c_text");
+var d_text = document.getElementById("d_text");
+var penalty = document.querySelectorAll(".option");
+var questions = document.getElementById("question");
+var continuequiz = document.querySelector(".submitanswer");
+var connect = document.getElementById("quiz")
+var score = document.querySelector("#score")
+
+
+startbutton.addEventListener("click", () => {
+    guidelines.style.display = "initial"
+})
+
+function beginTime() {
+    var timer = setInterval(function (){
+        timeremaining--;
+        countDown.textContent = timeremaining;
+        
+        if (timeremaining === 0 || attempt === theQuestions.length) {
+            clearInterval(timer);
+            timeout.style.display = "none"
+            scoresheet.style.display = "block"
+            scorehistory.textContent = timeremaining
+        } 
+    }, 1000)
+}
+
+nextbutton.addEventListener("click", () => {
+    timeout.style.display = 'block'
+    guidelines.removeAttribute("style")
+    startbutton.style.display = "none"
+
+    beginTime()
+    
+})
+
+submitanswers ()
+
+function submitanswers() {
+    wrong()
+    var attemptscore = theQuestions[attempt]
+    questions.innerText = attemptscore.question
+    
+    a_text.innerText = attemptscore.a
+    b_text.innerText = attemptscore.b
+    c_text.innerText = attemptscore.c
+    d_text.innerText = attemptscore.d
+}
+
+
+function wrong(){
+    penalty.forEach(penalty => penalty.checked = false)
+}
+
+function right() {
+    let answer
+    penalty.forEach(penalty => {
+        if(penalty.checked){
+            answer = penalty.id
+        }
+    })
+    return answer
+};
+
+continuequiz.addEventListener("click", (event) => {
+    console.log ("this button works")
+    event.preventDefault();
+    var result = right()
+    if(result) {
+        if (result === theQuestions[attempt].correct)
+        score++
+    }
+    attempt++
+
+    if(attempt < theQuestions.length) {
+        submitanswers()
+    } else {
+        connect.innerHTML = `
+        <h2>You have ${score}/${theQuestions.length} correct!</h2>
+        
+        `
+    }
+})
